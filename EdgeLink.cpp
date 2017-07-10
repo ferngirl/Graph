f59be5a37827587012c82aae757abc27877bc8a2
ferngirl <1378718912@qq.com>
2017-07-10 08:41:39 +0800

    graph storage
    图的实现，以及用邻接表来存储图

---
+++ EdgeLink.cpp
@@ -0,0 +1,94 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<vector>
+#include<assert.h>
+#include<iostream>
+using namespace std;
+
+//用邻接表存储图
+template<class W>
+struct EdgeLinkNode
+{
+	EdgeLinkNode(size_t src, size_t dst, W weight)
+	:_src(src)
+	, _dst(dst)
+	, _weight(weight)
+	{}
+
+	size_t _src;//起始位置
+	size_t _dst;//目标位置
+	W _weight;//权值
+	EdgeLinkNode<W>* _pNext;
+};
+
+template<class V,class W>
+class GraphLink
+{
+public:
+	GraphLink(const V* array, size_t size, bool isDirection=true)
+		:_vertrix(array,array+size)
+	{
+		_edges.resize(size);
+	}
+
+	size_t GetIndexOfVeritx(const V& v)
+	{
+		for (size_t idx = 0; idx < _vertrix.size(); ++idx)
+		{
+			if (_vertrix[idx] == v)
+				return idx;
+		}
+	}
+
+	void _AddrEdge(size_t src, size_t dst, W weight)
+	{
+		for (size_t idx = 0; idx < _edges.size(); ++idx)
+		{
+			EdgeLinkNode<W>* pCur = _edges[idx];
+			while (pCur)
+			{
+				if (pCur->_dst == dst)
+				{
+					pCur->_weight = weight;
+					return;
+				}
+				pCur = pCur->_pNext;
+			}
+		}
+		EdgeLinkNode<W>* pEdge = new EdgeLinkNode<W>(src,dst,weight);
+		pEdge->_pNext = _edges[src];
+		_edges[src] = pEdge;
+		if (_isDirection)
+		{
+			pEdge->_pNext = _edges[src];
+			_edges[src] = pEdge;
+		}
+	}
+
+	void AddEdge(const V& v1, const V& v2, W weight)
+	{
+		size_t src = GetIndexOfVeritx(v1);
+		size_t dst = GetIndexOfVeritx(v2);
+		assert(src != dst);
+		_AddrEdge(src, dst, weight);
+	}
+
+	void Print()
+	{
+		for (size_t idx = 0; idx < _edges.size(); ++idx)
+		{
+			EdgeLinkNode<W>* pCur = _edges[idx];
+			cout << _vertrix[idx] << "--->";
+			while (pCur)
+			{
+				cout << pCur->_dst << "[" << pCur->_weight << "]" << "--->";
+				pCur = pCur->_pNext;
+			}
+			cout << "NULL" << endl;
+		}
+	}
+private:
+	vector<V> _vertrix;
+	vector<EdgeLinkNode<W>*> _edges;
+	bool _isDirection;
+};
